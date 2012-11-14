e1e2a68ded26734cc53661e728d590e7a9a1b072
Ryan Senior <senior.ryan@gmail.com>
2012-11-14 10:56:21 -0600

    Switched delimited parser to use opencsv to parser delimited files with quotes. It's faster than using the existing regex.  When no quote is specified, it uses the existing regex approach.

--- cascading-core/src/test/java/cascading/scheme/util/DelimitedParserTest.java
+++ cascading-core/src/test/java/cascading/scheme/util/DelimitedParserTest.java
@@ -1,15 +1,18 @@
 package cascading.scheme.util;
 
-import java.util.regex.Pattern;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import cascading.tuple.Fields;
+
 public class DelimitedParserTest extends TestCase{
 
+	private DelimitedParser parserWithQuote = new DelimitedParser(",", "\"", null, true, true, true, Fields.ALL, Fields.ALL);
+	private DelimitedParser parserNoQuote = new DelimitedParser(",", null, null, true, true, true, Fields.ALL, Fields.ALL);
+	
 	@Test
 	public void testCreateSplitWithQuote() {
-		Pattern regex = DelimitedParser.createSplitPatternFor(",", "\"");
-		String []results = DelimitedParser.createSplit("abc,def,ghi", regex, 0);
+		String []results = parserWithQuote.createSplit("abc,def,ghi", 0);
 		assertEquals("abc", results[0]);
 		assertEquals("def", results[1]);
 		assertEquals("ghi", results[2]);
@@ -17,35 +20,31 @@ public class DelimitedParserTest extends TestCase{
 	
 	@Test
 	public void testCreateSplitWithQuotesInData() {
-		Pattern regex = DelimitedParser.createSplitPatternFor(",", "\"");
-		String []results = DelimitedParser.createSplit("\"abc\",def,\"ghi\"", regex, 0);
-		assertEquals("\"abc\"", results[0]);
+		String []results = parserWithQuote.createSplit("\"abc\",def,\"ghi\"", 0);
+		assertEquals("abc", results[0]);
 		assertEquals("def", results[1]);
-		assertEquals("\"ghi\"", results[2]);
+		assertEquals("ghi", results[2]);
 	}
 	
 	@Test
 	public void testCreateSplitWithQuotesAndCommasInData() {
-		Pattern regex = DelimitedParser.createSplitPatternFor(",", "\"");
-		String []results = DelimitedParser.createSplit("\"a,b,c\",def,\"g,h,i\"", regex, 0);
-		assertEquals("\"a,b,c\"", results[0]);
+		String []results = parserWithQuote.createSplit("\"a,b,c\",def,\"g,h,i\"", 0);
+		assertEquals("a,b,c", results[0]);
 		assertEquals("def", results[1]);
-		assertEquals("\"g,h,i\"", results[2]);
+		assertEquals("g,h,i", results[2]);
 	}
 	
 	@Test
 	public void testCreateSplitEscapedQuotes() {
-		Pattern regex = DelimitedParser.createSplitPatternFor(",", "\"");
-		String []results = DelimitedParser.createSplit("\"a\"\"b\"\"c\",def,\"ghi\"", regex, 0);
-		assertEquals("\"a\"\"b\"\"c\"", results[0]);
+		String []results = parserWithQuote.createSplit("\"a\\\"b\\\"c\",def,\"ghi\"", 0);
+		assertEquals("a\"b\"c", results[0]);
 		assertEquals("def", results[1]);
-		assertEquals("\"ghi\"", results[2]);
+		assertEquals("ghi", results[2]);
 	}
 	
 	@Test
 	public void testCreateSplitNoQuotes() {
-		Pattern regex = DelimitedParser.createSplitPatternFor(",", "");
-		String []results = DelimitedParser.createSplit("abc,def,ghi", regex, 0);
+		String []results = parserNoQuote.createSplit("abc,def,ghi", 0);
 		assertEquals("abc", results[0]);
 		assertEquals("def", results[1]);
 		assertEquals("ghi", results[2]);
